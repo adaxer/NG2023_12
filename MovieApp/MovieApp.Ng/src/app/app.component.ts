00ce0aaeb984ca5b0e9cadf817532dc3872d3765
Andreas Daxer <info@andreas-daxer.de>
2023-12-04 13:00:17 +0100

    MovieApp.Ng

--- MovieApp/MovieApp.Ng/src/app/app.component.ts
+++ MovieApp/MovieApp.Ng/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MatListModule],
   templateUrl: './app.component.html',
-  styles: [],
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'MovieApp.Ng';
