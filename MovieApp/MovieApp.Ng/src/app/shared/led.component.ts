2f4dde85a02adf473968a9c630b93c65093fff6e
Andreas Daxer <info@andreas-daxer.de>
2023-12-06 12:38:59 +0100

    Eigene Componente

---
+++ MovieApp/MovieApp.Ng/src/app/shared/led.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-led',
+  standalone: true,
+  templateUrl: './led.component.html',
+  styleUrls: []
+})
+export class LedComponent {
+  @Input() baseColor: string = '#AAAAAA';  // default to gray color
+  @Input() color: string = '#4CAF50';  // default to green color
+  @Output() colorChanged = new EventEmitter<string>();
+
+  resetColor(): void {
+    this.color = this.baseColor;
+    this.colorChanged.emit(this.color);
+  }
+
+  url = "https://jdslkfa";
+}
--- MovieApp/MovieApp.Ng/src/app/welcome.component.ts
+++ MovieApp/MovieApp.Ng/src/app/welcome.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { DecoratePipe } from './pipes/decorate.pipe';
 import { Weekdays } from './models/weekdays';
+import { LedComponent } from './shared/led.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [DecoratePipe],
+  imports: [DecoratePipe, LedComponent],
   templateUrl: './welcome.component.html',
   styles: ``
 })
 export class WelcomeComponent {
-Weekdays = Weekdays;
+  Weekdays = Weekdays;
+  ledColor = "#FFAA33";
+
+  componentChanged(newColor: string) {
+    console.log(newColor);
+  }
 }
