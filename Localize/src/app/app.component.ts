1411c732c56f69fe9b52ffdf2dbbbad96b42fcab
Andreas Daxer <info@andreas-daxer.de>
2023-12-07 13:29:31 +0100

    Localize Beispiel

---
+++ Localize/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styles: [],
+})
+export class AppComponent {
+  title = $localize `A localized App`;
+}
